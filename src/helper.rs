cbe04c9c915ba2db09ffda1c46902264ed86a233
MTRNord <mtrnord@users.noreply.github.com>
2017-05-23 19:35:21 +0200

    Implement basic Server without any features yet
    Signed-off-by: MTRNord <mtrnord1@gmail.com>

--- src/helper.rs
+++ src/helper.rs
@@ -1,17 +1,32 @@
 use std;
 use config::Config;
-pub fn start_config_dir() -> std::path::PathBuf {
+use std::path::Path;
+use std::io;
+
+// A simple implementation of `% touch path` (ignores existing files)
+// From http://rustbyexample.com/std_misc/fs.html
+fn touch(path: &Path) -> io::Result<()> {
+    use std::fs::OpenOptions;
+    match OpenOptions::new().create(true).write(true).open(path) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e),
+    }
+}
+
+pub fn get_config_dir() -> std::path::PathBuf {
     use app_dirs::*;
     const APP_INFO: AppInfo = AppInfo { name: "IMAPServer", author: "MTRNord" };
     let config_root = app_root(AppDataType::UserConfig, &APP_INFO).unwrap();
-    println!("{:?}", config_root);
     config_root
 }
 
 pub fn get_config() -> Config{
     use config::{File, FileFormat};
+    let mut config_dir = get_config_dir();
+    config_dir.push("Main.yml");
+    touch(config_dir.as_path());
     let mut c = Config::new();
-    // Add 'Main.json'
-    c.merge(File::new("Main", FileFormat::Json).required(false)).unwrap();
+    // Add 'Main.yaml'
+    c.merge(File::new(config_dir.to_str().unwrap(), FileFormat::Yaml).required(true)).unwrap();
     c
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,112 @@
 extern crate app_dirs;
 extern crate config;
+extern crate bytes;
+extern crate futures;
+extern crate tokio_io;
+extern crate tokio_proto;
+extern crate tokio_service;
+use std::io;
+use std::str;
+use bytes::BytesMut;
+use tokio_io::codec::{Encoder, Decoder};
+use tokio_proto::pipeline::ServerProto;
+use tokio_io::{AsyncRead, AsyncWrite};
+use tokio_io::codec::Framed;
+use tokio_service::Service;
+use futures::{future, Future, BoxFuture};
+use tokio_proto::TcpServer;
+use std::net::SocketAddr;
+
+pub struct ImapCodec;
+impl Decoder for ImapCodec {
+    type Item = String;
+    type Error = io::Error;
+
+    fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<String>> {
+        if let Some(i) = buf.iter().position(|&b| b == b'\n') {
+            // remove the serialized frame from the buffer.
+            let line = buf.split_to(i);
+
+            // Also remove the '\n'
+            buf.split_to(1);
+
+            // Turn this data into a UTF string and return it in a Frame.
+            match str::from_utf8(&line) {
+                Ok(s) => Ok(Some(s.to_string())),
+                Err(_) => Err(io::Error::new(io::ErrorKind::Other,
+                                             "invalid UTF-8")),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl Encoder for ImapCodec {
+    type Item = String;
+    type Error = io::Error;
+
+    fn encode(&mut self, msg: String, buf: &mut BytesMut) -> io::Result<()> {
+        buf.extend(msg.as_bytes());
+        buf.extend(b"\n");
+        Ok(())
+    }
+}
+
+pub struct ImapProto;
+impl<T: AsyncRead + AsyncWrite + 'static> ServerProto<T> for ImapProto {
+    /// For this protocol style, `Request` matches the `Item` type of the codec's `Encoder`
+    type Request = String;
+
+    /// For this protocol style, `Response` matches the `Item` type of the codec's `Decoder`
+    type Response = String;
+
+    /// A bit of boilerplate to hook in the codec:
+    type Transport = Framed<T, ImapCodec>;
+    type BindTransport = Result<Self::Transport, io::Error>;
+    fn bind_transport(&self, io: T) -> Self::BindTransport {
+        Ok(io.framed(ImapCodec))
+    }
+}
+
+pub struct Imap;
+impl Service for Imap {
+    // These types must match the corresponding protocol types:
+    type Request = String;
+    type Response = String;
+
+    // For non-streaming protocols, service errors are always io::Error
+    type Error = io::Error;
+
+    // The future for computing the response; box it for simplicity.
+    type Future = BoxFuture<Self::Response, Self::Error>;
+
+    // Produce a future for computing a response from a request.
+    fn call(&self, req: Self::Request) -> Self::Future {
+        if req.contains("CAPABILITY")  {
+            let res = "* CAPABILITY IMAP4rev1 AUTH=PLAIN LOGINDISABLED".to_string();;
+            future::ok(res).boxed()
+        }else {
+            // In this case, the response is immediate.
+            future::ok(req).boxed()
+        }
+
+    }
+}
+
 fn main() {
-    helper::start_config_dir();
-    helper::get_config().set_default("RFC", "3501");
-    println!("Hello, world!");
+    let mut config = helper::get_config();
+    config.set_default("RFC", "3501").unwrap();
+    config.set_default("address", "127.0.0.1:143").unwrap();
+    // Specify the localhost address
+    let addr: SocketAddr =  config.get_str("address").unwrap().parse().unwrap();
+
+    // The builder requires a protocol and an address
+    let server = TcpServer::new(ImapProto, addr);
+
+    // We provide a way to *instantiate* the service for each new
+    // connection; here, we just immediately return a new instance.
+    server.serve(|| Ok(Imap));
 }
 
 mod helper;
