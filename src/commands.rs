bfb3ce68f39e9a74f5df7901c1f7dcff9bd9e59e
MTRNord <mtrnord1@gmail.com>
2017-06-09 15:13:52 +0200

    Add fake login command
    Signed-off-by: MTRNord <mtrnord1@gmail.com>

--- src/commands.rs
+++ src/commands.rs
@@ -45,14 +45,34 @@ pub fn logout<'a>(mut conns: std::cell::RefMut<'a, std::collections::HashMap<std
             println!("{}", "* BYE IMAP4rev1 Server logging out\r\n");
             println!("{}{}", identifier, " OK LOGOUT completed");
         } else {
-            tx.send(format!("{}", "* BYE IMAP4rev1 Server logging out\r\n")).unwrap();
+            //tx.send(format!("{}", "* BYE IMAP4rev1 Server logging out\r\n")).unwrap();
 
             //Print to view for debug
-            println!("{}", "* BYE IMAP4rev1 Server logging out\r\n");
+            //println!("{}", "* BYE IMAP4rev1 Server logging out\r\n");
         }
     }
 }
 
 pub fn login<'a>(mut conns: std::cell::RefMut<'a, std::collections::HashMap<std::net::SocketAddr, futures::sync::mpsc::UnboundedSender<std::string::String>>>, msg: std::string::String, addr: &'a std::net::SocketAddr) {
-    unimplemented!();
+    // For each open connection except the sender, send the
+    // string via the channel.
+    let iter = conns
+        .iter_mut()
+        .map(|(y, v)| (y, v));
+
+    let mut identifier_iter = msg.split_whitespace();
+    let identifier = identifier_iter.nth(0).unwrap();
+    for (y, tx) in iter {
+        if y == addr {
+            tx.send(format!("{} {}", identifier, "OK LOGIN completed")).unwrap();
+
+            //Print to view for debug
+            println!("{} {}", identifier, "OK LOGIN completed");
+        } else {
+            //tx.send(format!("{}", "* BYE IMAP4rev1 Server logging out\r\n")).unwrap();
+
+            //Print to view for debug
+            //println!("{}", "* BYE IMAP4rev1 Server logging out\r\n");
+        }
+    }
 }
