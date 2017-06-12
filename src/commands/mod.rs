e2c977f06fc587d11c49f451083a27672959420b
Marcel <mtrnord1@gmail.com>
2017-06-12 09:30:44 +0200

    maybe fix list

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -38,11 +38,11 @@ pub fn list<'a>(mut conns: std::cell::RefMut<'a, std::collections::HashMap<std::
     let identifier = identifier_iter.nth(0).unwrap();
     for (y, tx) in iter {
         if y == addr {
-            tx.send(format!("{}", "* LIST (\\Noselect) \"/\" \"\"\r\n")).unwrap();
+            tx.send(format!("{}", "* LIST () \"/\" INBOX\r\n")).unwrap();
             tx.send(format!("{}{}", identifier, "OK LIST Completed\r\n")).unwrap();
 
             //Print to view for debug
-            println!("{}", "* LIST (\\Noselect) \"/\" \"\"\r\n");
+            println!("{}", "* LIST () \"/\" INBOX\r\n");
             println!("{}{}", identifier, "OK LIST Completed\r\n");
         }
     }
