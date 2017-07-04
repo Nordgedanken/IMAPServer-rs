eed5812410a91e4cc5fc38c315b1dee9762bb584
Marcel <mtrnord1@gmail.com>
2017-07-04 12:54:54 +0200

    fix parsing base64
    Signed-off-by: Marcel <mtrnord1@gmail.com>

--- src/commands/authenticate.rs
+++ src/commands/authenticate.rs
@@ -43,7 +43,8 @@ pub fn parse_login_data <'a>(
     use base64::decode;
     let bytes = decode(args[0]).unwrap();
     let string = match String::from_utf8(bytes){
-        Ok(v) => v
+        Ok(v) => v,
+        Err(e) => Err(format!("Invalid UTF-8 sequence: {}", e)),
     };
     println!("{}", string);
 }
