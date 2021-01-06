42f159876eebf023eb04e11c9c29f2e8c75ce767
MTRNord <mtrnord1@gmail.com>
2021-01-06 22:23:41 +0100

    feat: Add user functions to cli
    Took 16 minutes

--- imap-server/src/cli/mod.rs
+++ imap-server/src/cli/mod.rs
@@ -3,5 +3,23 @@ use crate::passwords;
 
 pub async fn add(db: Database, username: String, pass: String) -> color_eyre::Result<()> {
     let hash = passwords::hash(pass.as_str());
+    db.add_user(username, hash).await?;
+    tracing::info!("Successfully added User!");
+    Ok(())
+}
+
+pub async fn remove(db: Database, username: String) -> color_eyre::Result<()> {
+    db.remove_user(username).await?;
+    tracing::info!("Successfully removed User!");
+    Ok(())
+}
+
+pub async fn change_password(
+    db: Database,
+    username: String,
+    pass: String,
+) -> color_eyre::Result<()> {
+    db.add_user(username, pass).await?;
+    tracing::info!("Successfully changed user's password!");
     Ok(())
 }
--- imap-server/src/main.rs
+++ imap-server/src/main.rs
@@ -1,4 +1,4 @@
-use clap::clap_app;
+use clap::{clap_app, ArgMatches};
 use color_eyre::eyre::Result;
 
 mod cli;
@@ -15,29 +15,59 @@ async fn main() -> Result<()> {
         .with_thread_names(true)
         .with_max_level(tracing::Level::INFO)
         .init();
-    tracing::info!("Starting...");
     let opts = clap_app!("imap-server" =>
         (about: "A Rust imap server")
         (@arg CONFIG: -c --config +takes_value "Sets a custom config file")
         (@arg verbose: -v --verbose "Print test information verbosely")
-        (@subcommand "add-user" =>
-            (about: "adds a user")
-            (@arg USERNAME: -u --username +takes_value +required "The username of the new user")
-            (@arg PASSWORD: -p --password +takes_value +required "The password of the new user")
+        (@subcommand "user" =>
+            (@subcommand "add" =>
+                (about: "adds a user")
+                (@arg USERNAME: -u --username +takes_value +required "The username of the new user")
+                (@arg PASSWORD: -p --password +takes_value +required "The password of the new user")
+            )
+            (@subcommand "change-password" =>
+                (about: "changes the password of the given user")
+                (@arg USERNAME: -u --username +takes_value +required "The username of the new user")
+                (@arg PASSWORD: -p --password +takes_value +required "The password of the new user")
+            )
+            (@subcommand "remove" =>
+                (@arg USERNAME: -u --username +takes_value +required "The username of the new user")
+                (about: "removes a user")
+            )
+            (about: "user operations")
         )
     )
     .get_matches();
 
     let database = database::Database::open().await?;
     match opts.subcommand() {
-        Some(("add-user", add_user)) => {
-            if let Some(username) = add_user.value_of("USERNAME") {
-                if let Some(password) = add_user.value_of("PASSWORD") {
-                    cli::add(database, username.to_string(), password.to_string()).await?;
+        Some(("user", user)) => match user.subcommand() {
+            Some(("add", add_user)) => {
+                if let Some(username) = add_user.value_of("USERNAME") {
+                    if let Some(password) = add_user.value_of("PASSWORD") {
+                        cli::add(database, username.to_string(), password.to_string()).await?;
+                    }
                 }
             }
+            Some(("change-password", add_user)) => {
+                if let Some(username) = add_user.value_of("USERNAME") {
+                    if let Some(password) = add_user.value_of("PASSWORD") {
+                        cli::change_password(database, username.to_string(), password.to_string())
+                            .await?;
+                    }
+                }
+            }
+            Some(("remove", remove_user)) => {
+                if let Some(username) = remove_user.value_of("USERNAME") {
+                    cli::remove(database, username.to_string()).await?;
+                }
+            }
+            _ => {}
+        },
+        _ => {
+            tracing::info!("Starting...");
+            server::run(database).await?
         }
-        _ => server::run(database).await?,
     }
     Ok(())
 }
